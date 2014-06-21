18db9ae3d74a42f5e8ff9e7128c7d53b26000d01
Takuma BraveJohnny Miyake <takuma.miyake@framgia.com>
2014-06-21 14:02:19 +0900

    Allowing numeric string to specify a target position.

---
+++ lib/mongoid/orderable/errors.rb
@@ -0,0 +1,2 @@
+require 'mongoid/orderable/errors/mongoid_orderable_error'
+require 'mongoid/orderable/errors/invalid_target_position'
---
+++ lib/mongoid/orderable/errors/mongoid_orderable_error.rb
@@ -0,0 +1,14 @@
+module Mongoid::Orderable
+  module Errors
+    class MongoidOrderableError < ::Mongoid::Errors::MongoidError
+
+      if MongoidOrderable.mongoid2?
+        def translate key, options
+          [:message, :summary, :resolution].map do |section|
+            ::I18n.translate "#{BASE_KEY}.#{key}.#{section}", options
+          end.join ' '
+        end
+      end
+    end
+  end
+end
