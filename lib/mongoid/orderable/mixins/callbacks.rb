e2baa2c3c785f41b731a9c0ed9fa4086e329a330
shields <shields@tablecheck.com>
2021-01-24 19:08:19 +0900

    - Feature: Add transaction support with lock table - Feature: Avoid calling position function if neither position nor scope is changing - Fix: Do not run embedded destroy callbacks when the root object has been destroyed - Fix: Global config should allow :as parameter - CI: Travis should use Mongo 4.4 in replica set configuration

--- lib/mongoid/orderable/mixins/callbacks.rb
+++ lib/mongoid/orderable/mixins/callbacks.rb
@@ -6,75 +6,21 @@ module Mixins
   module Callbacks
     extend ActiveSupport::Concern
 
+    ORDERABLE_TRANSACTION_KEY = :__mongoid_orderable_in_txn
+
     included do
-      before_save :add_to_list
-      after_destroy :remove_from_list
+      around_save :orderable_update_positions
+      after_destroy :orderable_remove_positions, unless: -> { embedded? && _root.destroyed? }
+
+      delegate :update_positions,
+               :remove_positions,
+               to: :orderable_engine,
+               prefix: :orderable
 
       protected
 
-      def add_to_list
-        orderable_keys.each do |field|
-          apply_position(field, move_all[field])
-        end
-      end
-
-      def remove_from_list
-        orderable_keys.each do |field|
-          remove_position_from_list(field)
-        end
-      end
-
-      def remove_position_from_list(field)
-        col = orderable_field(field)
-        pos = orderable_position(field)
-        orderable_scope(field).where(col.gt => pos).inc(col => -1)
-      end
-
-      def apply_position(field, target_position)
-        if persisted? && !embedded? && orderable_scope_changed?(field)
-          self.class.unscoped.find(_id).remove_position_from_list(field)
-          set(field => nil)
-        end
-
-        return if !target_position && in_list?(field)
-
-        target_position = resolve_target_position(field, target_position)
-        scope = orderable_scope(field)
-        col = orderable_field(field)
-        pos = orderable_position(field)
-
-        if !in_list?(field)
-          scope.gte(col => target_position).inc(col => 1)
-        elsif target_position < pos
-          scope.where(col.gte => target_position, col.lt => pos).inc(col => 1)
-        elsif target_position > pos
-          scope.where(col.gt => pos, col.lte => target_position).inc(col => -1)
-        end
-
-        if persisted?
-          set(field => target_position)
-        else
-          send("orderable_#{field}_position=", target_position)
-        end
-      end
-
-      def resolve_target_position(field, target_position)
-        target_position ||= :bottom
-
-        unless target_position.is_a? Numeric
-          target_position = case target_position.to_s
-                            when 'top' then orderable_top(field)
-                            when 'bottom' then orderable_bottom(field)
-                            when 'higher' then orderable_position(field).pred
-                            when 'lower' then orderable_position(field).next
-                            when /\A\d+\Z/ then target_position.to_i
-                            else raise Mongoid::Orderable::Errors::InvalidTargetPosition.new(target_position)
-                            end
-        end
-
-        target_position = orderable_top(field) if target_position < orderable_top(field)
-        target_position = orderable_bottom(field) if target_position > orderable_bottom(field)
-        target_position
+      def orderable_engine
+        @orderable_engine ||= Mongoid::Orderable::Engine.new(self)
       end
     end
   end
